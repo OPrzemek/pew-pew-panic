42d07af5fb001ea9f0b755070afc6dfcea1db69b
Beata Patzer <beatkapatzer@gmail.com>
2025-05-09 20:28:15 +0200

    added weapon

---
+++ Assets/Scripts/Player/Projectile.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //todo: dettect:D colisioin with enemies
+    }
+}
--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -2,9 +2,30 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public int projectilesPerShot = 3;
+    public float spreadAngle = 10f;
+    public GameObject projectilePrefab;
+
+    public void CustomUpdate()
+
+    //Obracanie broni wokó³ statku
+    {
+
+    }
     //strzelanie
     public void Shoot()
     {
+        for(int i = 0; i < projectilesPerShot; i++)//Powtórka tyle razy, ile pocisków chce sie wystrzeliæ
+        {
+            //Jesli jest wiecej ni¿ 1 pocisk, losuje siê rozrzut (prawo/lewo), jesli jest tylko 1 to nie ma rozrzutu
+            float spread = (projectilesPerShot > 1) ? Random.Range(-spreadAngle, spreadAngle) : 0f;
+            //dodanie pocisku
+            GameObject projectile = Instantiate(projectilePrefab,transform);
+            //nadanie predkosci liniowej cia³a fizycznemego pocisku
+            Rigidbody2D rbProjectile= projectile.GetComponent<Rigidbody2D>();
+            Debug.Log(gameObject.transform.forward);
+            rbProjectile.linearVelocity= gameObject.transform.up*spread;
+        }
 
     }
 }
